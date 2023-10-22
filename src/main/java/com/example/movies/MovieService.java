5abae8d282edaf6043ee2c4b97930d36df5ab43f
HYPERION\David <david.tonghw@gmail.com>
2023-10-22 21:03:18 +0800

    spring boot mongodb and java mini project

---
+++ src/main/java/com/example/movies/MovieService.java
@@ -0,0 +1,25 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    //uses repo class to talk to database
+    public List<Movie> findAllMovies() {
+       return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+}
